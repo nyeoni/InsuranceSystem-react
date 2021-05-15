64d74337b3be249072c8c204804fe9d2c81548d5
Chloekkk <skdusdl8804@gmail.com>
2021-05-15 17:38:30 +0900

    modify : Login to main page

--- src/App.js
+++ src/App.js
@@ -1,78 +1,19 @@
 import React from "react";
-import {BrowserRouter, Switch, Route, Link, Redirect, useHistory, withRouter} from "react-router-dom";
-import Login from "./pages/Login";
+import {BrowserRouter, Switch, Route, Redirect, withRouter} from "react-router-dom";
+import {default as Login} from "./pages/Login";
 import Profile from "./pages/Profile";
-import { signIn } from "./pages/Auth";
-import {useState} from "react";
+
 import Base from "./pages/Base";
-import apiAxios from "./apiAxios";
-import AuthRoute from "./pages/AuthRoute";
+
 
 function App() {
-    const [user, setUser] = useState({
-        name: '', department: '', phoneNumber: '', role: '', email: ''
-    });
-    const authenticated = user.name != ''
-
-    const [info, setInfo] = useState({
-        id: '',
-        password: ''
-    });
-    const history = useHistory();
-
-    const {id, password} = info;
-
-    const onChange = e => {
-        const {name, value} = e.target;
-        setInfo({
-            ...info,
-            [name]: value
-        });
-    };
-
-    function validateUser(data) {
-        if (!(data.error))
-        {
-            setUser(
-                {
-                    name: data.name,
-                    department: data.department,
-                    phoneNumber: data.phoneNumber,
-                    role: data.role,
-                    email: data.email
-                }
-            )
-        }
-        else
-        {
-            alert("아이디 혹은 비밀번호가 잘못되었습니다!");
-        }
-    }
-
-    const login = () => {
-        // setUser(signIn({id, password}));
-        apiAxios('employee/login', validateUser, info)
-        console.log(user);
-        if (authenticated) {
-            alert("됐는데에에!!");
-            console.log(user);
-            // <Route path="/" exact={true} render={props => true ? <Profile /> : (<Redirect to={{ pathname: "/login"}} />) } />
-            history.push("/home");
-        }
-        else {
-            console.log("nononon");
-        }
-    };
-
-    const logout = () => setUser(null);
-
     return (
         <BrowserRouter>
             <main className="App">
                 <Switch>
-                    <Route path="/login" exact={true} render={props => <Login id={id} password={password} onChange={onChange} login={login}/>} />
-                    <Route path="/" exact={true} render={props => true ? <Profile /> : (<Redirect
-                        to={{ pathname: "/login"}} />) } />
+                    <Route path="/login" exact={true} render={props => <Login />} />
+                    {/*<Route path="/" exact={true} render={props => true ? <Login /> : (<Redirect*/}
+                    {/*    to={{ pathname: "/login"}} />) } />*/}
                     <Route paht="/home" exact={true} render={() => <Base />} />
                 </Switch>
             </main>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,89 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-
 import '../css/Login.css';
+import {Route, Redirect, useHistory} from "react-router-dom";
+import apiAxios from "../apiAxios";
+import Base from "./Base";
 
-function Login({id, password, onChange, login}){
-        return (
-            <div className="wrap">
-                <div className="panel left-side">
-                    <div className="left-logo">
-                    </div>
+const Login = () => {
+    const [user, setUser] = useState({
+        name: '', department: '', phoneNumber: '', role: '', email: ''
+    });
+    const [info, setInfo] = useState({
+        id: '',
+        password: ''
+    });
+    const authenticated = user.name != ''
+    const {id, password} = info;
+    const history = useHistory();
+
+    //functions
+
+    const onChange = e => {
+        const {name, value} = e.target;
+        setInfo({
+            ...info,
+            [name]: value
+        });
+    };
+
+    function validateUser(data) {
+        console.log("vlidate");
+        if (!(data.error)) {
+            setUser(
+                {
+                    name: data.name,
+                    department: data.department,
+                    phoneNumber: data.phoneNumber,
+                    role: data.role,
+                    email: data.email
+                }
+            )
+            history.replace("/");
+        } else {
+            alert("아이디 혹은 비밀번호가 잘못되었습니다!");
+        }
+    }
+    const login = () => {
+        apiAxios('employee/login', validateUser, info)
+    };
+    const logout = () => setUser(null);
+    return (
+        <div className="wrap">
+            <div className="panel left-side">
+                <div className="left-logo">
                 </div>
-                <div className="panel right-side">
-                    <div className="login-container">
-                        <div className="right-title">
-                            <label>HM 손해보험</label>
-                            <h3>보험 정보 시스템</h3>
-                        </div>
-                        <div className="login-div">
-                            <div className="square-box">
-                                <div className="square-img">
-                                    <div className="id-img"></div>
-                                </div>
-                                <input onChange={onChange} value={id} name="id" className="id-input" type="text" id="userid" placeholder="ID"></input>
+            </div>
+            <div className="panel right-side">
+                <div className="login-container">
+                    <div className="right-title">
+                        <label>HM 손해보험</label>
+                        <h3>보험 정보 시스템</h3>
+                    </div>
+                    <div className="login-div">
+                        <div className="square-box">
+                            <div className="square-img">
+                                <div className="id-img"></div>
                             </div>
-                            <div className="square-box">
-                                <div className="square-img">
-                                    <div className="pass-img"></div>
-                                </div>
-                                <input onChange={onChange} value={password} name="password" className="pass-input" type="password" id="password" placeholder="Password"></input>
+                            <input onChange={onChange} value={id} name="id" className="id-input" type="text" id="userid"
+                                   placeholder="ID"></input>
+                        </div>
+                        <div className="square-box">
+                            <div className="square-img">
+                                <div className="pass-img"></div>
                             </div>
-                            <button onClick={login} className="square" id="login">로그인</button>
+                            <input onChange={onChange} value={password} name="password" className="pass-input"
+                                   type="password" id="password" placeholder="Password"></input>
                         </div>
-                        <div>
-                            <a id="forget-pass">비밀번호를 잊으셨습니까?</a>
-                        </div>
+                        <button onClick={login} className="square" id="login">로그인</button>
+                    </div>
+                    <div>
+                        <a id="forget-pass">비밀번호를 잊으셨습니까?</a>
                     </div>
                 </div>
             </div>
-        );
+        </div>
+    );
 }
 
 export default Login;
