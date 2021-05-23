61e1d7a6f57c2ac5c4f18a44cb96832ae8ae90c6
Chloekkk <skdusdl8804@gmail.com>
2021-05-24 03:26:44 +0900

    complete : board main and detail pages

--- src/pages/Home/BoardDetail.jsx
+++ src/pages/Home/BoardDetail.jsx
@@ -2,15 +2,41 @@ import React from "react";
 import {Wrapper} from "../../components/Wrapper";
 import '../../css/Detail.css';
 import {GeneralTable} from "../../components/GeneralTable";
+import useAsync from "../../customHooks/useAsync";
+import {useHistory} from "react-router-dom";
+import axios from "axios";
+import { Spin } from 'antd';
+import {Detail} from "../../components/Detail";
+
+async function getBoard(id) {
+    const response = await axios.get(
+        `https://608c26ef9f42b20017c3d801.mockapi.io/api/v1/board/${id}`
+    );
+    return response.data;
+}
 
 const BoardDetail = ({ match }) => {
     const { id } = match.params;
+    const [state] = useAsync(() => getBoard(id), [id]);
+    const { loading, data: board, error } = state;
+    const history = useHistory();
 
-    console.log(id);
+    if (error) return <div>에러가 발생했습니다</div>;
+    if (!board || loading) {
+        return(
+            <Wrapper>
+                <Spin style={{textAlign: "center", width: "100%", height: "100%", marginTop: "200px"}}/>
+            </Wrapper>
+        );
+    }
+
     return (
-        <Wrapper title="공지사항" underline={true}>
-            <GeneralTable />
+        <Wrapper>
+            <Detail board={board}/>
         </Wrapper>
+        // <Wrapper title={board.title} underline={true}>
+        //     <GeneralTable />
+        // </Wrapper>
     )
 }
 
