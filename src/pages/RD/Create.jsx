fd3e755b1575d5dedf8f7d252b18a84691566e17
Chloekkk <skdusdl8804@gmail.com>
2021-06-05 18:21:22 +0900

    merge

--- src/pages/RD/Create.jsx
+++ src/pages/RD/Create.jsx
@@ -1,24 +1,30 @@
-import React, {useState} from "react";
-import {Form, Input, Button, Select,InputNumber} from 'antd';
+import React, {useEffect, useState} from "react";
+import {Form, Input, Button, Select,InputNumber, Row, Col} from 'antd';
 import axios from "axios";
 import {Wrapper} from "../../components/Wrapper";
 import "../../css/Detail.css";
-
-//     "status": 1
-//todo: 최저 연령, 최고 연령, 기초 보험요율 api
+import {useSelector} from "react-redux";
 
 const Create = () => {
     const title = "상품개발"
     const subtitle = "HM 손해보험의 보험상품을 개발하기 위한 페이지입니다."
+    // const userId = useSelector(state => (state.user.data.id))
+
     const [state, setState] = useState({
-        id: '',
         name: '',
-        type: '',
         description: '',
-        liablityCoverages: [],
+        coverages: [],
+        registerDocuments: [],
         accidentDocuments: [],
-        status : '1'
+        basePremiumRate: '',
+        startAge: '',
+        endAge: '',
+        creditRating: '',
+        category: '',
+        createEmployeeId : '1234',
+        managementEmployeeId : ''
     })
+
     const handleChange = (event) =>{
         const target = event.target;
         const name = target.name;
@@ -26,79 +32,126 @@ const Create = () => {
         if(Array.isArray(value)){
             setState({
                 ...state,
-                [name] : [...value]
+                [name] : [...value] //[target.startAge]
             });
             console.log('array val', value)
         }else{
-            setState({
-                ...state,
-                [name]: value});
+            setState({...state, [name]: value});
             console.log('single val', value)
         }
+        // console.log('state, ', state);
     }
+    useEffect(() => {
+        console.log('useEffect ',state);
+    }, [state])
+
     const postInsurance = () => {
-        const url = 'https://608c26ef9f42b20017c3d801.mockapi.io/api/v1/insurance';
+        const url = 'http://hminsu.net//api/insurance/create';
+        const {name, description,coverages, registerDocuments, accidentDocuments,basePremiumRate,category,
+            startAge, endAge, creditRating,createEmployeeId,managementEmployeeId }= state;
         axios.post(url, {
-            id: state.id,
-            name: state.name,
-            type: state.type,
-            description: state.description,
-            liablityCoverages: state.liablityCoverages,
-            accidentDocuments: state.accidentDocuments,
-            status: '1'
-        }).then(r => console.log(r));
+            name,
+            description,
+            coverages,
+            registerDocuments,
+            accidentDocuments,
+            basePremiumRate,
+            category,
+            createEmployeeId,
+            managementEmployeeId,
+            target : {startAge, endAge, creditRating},
+        }).then(r => {console.log(r)
+            alert("api 성공")
+        });
     }
     const handleSubmit = () => {
-        postInsurance().then((response) => {console.log('response, ', response.data)})
+        postInsurance()
+            // .then((response) => {console.log('response, ', response.data)})
     }
     return (
         <Wrapper title={title} subtitle={subtitle} underline={true}>
-            <Form labelCol={{span: 10,}} wrapperCol={{span: 12,}} layout="vertical" scrollToFirstError={true} onFinish={handleSubmit}>
-                <Form.Item required={true} label="보험상품 이름" ><Input name="name" value={state.name} onChange={handleChange} placeholder="예시) XX 자동차 보험"/></Form.Item>
-                <Form.Item required={true} label="보험상품 ID번호" ><Input name="id" value={state.id} onChange={handleChange} placeholder="예시) xxxx-xxxx"/></Form.Item>
+            <Form labelCol={{span: 10,}} wrapperCol={{span: 14,}} layout="vertical" size={"large"} onFinish={handleSubmit}>
+                <Form.Item rules={[{required: true, message: '보험의 이름을 입력해주세요!'}]} name="name" label="보험상품 이름" >
+                    <Input name="name" value={state.name} onChange={handleChange} placeholder="예시) XX 자동차 보험"/>
+                </Form.Item>
 
-                <Form.Item required={true} label="상품 항목">
-
-                    <Select value={state.type} onChange={(val)=>{handleChange({target: {name: 'type', value: val}})}}>
-                        <Select.Option value="Car">자동차 보험</Select.Option>
-                        <Select.Option value="Driver">운전자 보험</Select.Option>
-                        <Select.Option value="Fire">화재 보험</Select.Option>
-                        <Select.Option value="Traveller">여행자 보험</Select.Option>
+                <Form.Item rules={[{required: true, message: '상품의 종류를 선택해주세요!'}]} name='category' label="상품 항목">
+                    <Select value={state.category} onChange={(val)=>{handleChange({target: {name: 'category', value: val}})}}>
+                        <Select.Option value="자동차">자동차 보험</Select.Option>
+                        <Select.Option value="운전자">운전자 보험</Select.Option>
+                        <Select.Option value="화재">화재 보험</Select.Option>
+                        <Select.Option value="여행자">여행자 보험</Select.Option>
                     </Select>
                 </Form.Item>
-                <Form.Item label="가입 연령대" style={{ marginBottom: 0 }}>
-                    <Form.Item >
-                        <Input style={{ display: 'inline-block', width: 'calc(50% - 4px)', marginInlineEnd:'8px'}} name="minimum" placeholder="가입 최저 연령을 입력해주세요"/>
-                        <Input style={{ display: 'inline-block', width: 'calc(50% - 4px)'}} name="maximum" placeholder="가입 최고 연령을 입력해주세요" />
-                    </Form.Item>
-                </Form.Item>
+                <Row>
+                    <Col span={7}>
+                        <Form.Item wrapperCol={12} label="가입 연령대">
+                            <InputNumber style={{ display: 'inline-block', width: '45%', marginInlineEnd:'4px'}} placeholder="가입 최저 연령"
+                                         min={0} max={100.00} name="startAge" value={state.startAge}
+                                         onChange={(val)=>{handleChange({target: {name: 'startAge', value: val}})}}/>
 
-                <Form.Item label="보험 상품의 보장내용" required={true} rules={[{message: '하나 이상의 제출 서류를 선택해주세요', type:'array'}]}>
-                    <Select mode="multiple" value={state.liablityCoverages} onChange={(val)=>{handleChange({target: {name: 'liablityCoverages', value: val}})}}
+                            <InputNumber style={{ display: 'inline-block', width: '45%'}} placeholder="가입 최고 연령"
+                                         min={0} max={100.00} name="endAge" value={state.endAge}
+                                         onChange={(val)=>{handleChange({target: {name: 'endAge', value: val}})}}/>
+                        </Form.Item>
+                    </Col>
+                    <Col span={7}>
+                        <Form.Item wrapperCol={12} name={"creditRating"} label="최소 신용등급">
+                            <InputNumber style={{ display: 'inline-block', width: '100%'}}
+                                   min={1} max={10} step="1" name="creditRating" placeholder="1~10등급" value={state.creditRating}
+                                   onChange={(val)=>{handleChange({target: {name: 'creditRating', value: val}})}}/>
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Form.Item label="보험 상품의 보장내용" name={'coverages'} rules={[{required:true, message: '하나 이상의 보장내용을 선택해주세요', type:'array'}]}>
+                    <Select mode="multiple" value={state.coverages} onChange={(val)=>{handleChange({target: {name: 'coverages', value: val}})}}
                             placeholder="보험 상품의 사고 보장내용을 선택해주세요">
-                        <Select.Option value="bodilyCoverage">대인배상</Select.Option>
-                        <Select.Option value="propertyCoverage">대물배상</Select.Option>
-                        <Select.Option value="selfAccident">자기신체사고</Select.Option>
+                        <Select.Option value="대인배상">대인배상</Select.Option>
+                        <Select.Option value="대물배상">대물배상</Select.Option>
+                        <Select.Option value="자기신체사고">자기신체사고</Select.Option>
                     </Select>
                 </Form.Item>
 
-                <Form.Item label="보험 가입시 요구 제출 서류" required={true} rules={[{ message: '하나 이상의 제출 서류를 선택해주세요', type:'array' }]}>
+                <Form.Item label="보험 가입시 요구 제출 서류" name={'registerDocuments'} rules={[{required: true, message: '하나 이상의 제출 서류를 선택해주세요', type:'array' }]}>
+                    <Select mode="multiple" value={state.registerDocuments} onChange={(val)=>{handleChange({target: {name: 'registerDocuments', value: val}})}}
+                            placeholder="보험 가입시 필요한 제출 서류를 선택해주세요">
+                        <Select.Option value="운전면허">운전면허</Select.Option>
+                        <Select.Option value="운전자 사고 이력">운전자 사고 이력</Select.Option>{/*option??*/}
+                        <Select.Option value="자동차 등록증">자동차 등록증</Select.Option>
+                        <Select.Option value="건물 등록 문서">건물 등록 문서</Select.Option>
+                        <Select.Option value="여권사본">여권사본</Select.Option>
+                        <Select.Option value="항공 탑승권 등 여행 증명서류">항공 탑승권 등 여행 증명서류</Select.Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item rules={[{required: true, message: '하나 이상의 제출 서류를 선택해주세요', type:'array' }]} name={'accidentDocuments'} label="사고 보상청구시 요구 제출 서류">
                     <Select mode="multiple" value={state.accidentDocuments} onChange={(val)=>{handleChange({target: {name: 'accidentDocuments', value: val}})}}
                             placeholder="보험 가입시 필요한 제출 서류를 선택해주세요">
-                        <Select.Option value="driverLicense">운전면허</Select.Option>
-                        <Select.Option value="accidentHistory">운전자 사고 이력</Select.Option>{/*option??*/}
-                        <Select.Option value="carRegistration">자동차 등록증</Select.Option>
-                        <Select.Option value="building">집 문서??ㅋ</Select.Option>
-                        <Select.Option value="passport">여권사본</Select.Option>
-                        <Select.Option value="flightTicket">항공 탑승권 등 여행증명서류</Select.Option>
+                        <Select.Option value="사고 처리 협력업체 영수증">사고 처리 협력업체 영수증</Select.Option>
+                        <Select.Option value="자동차 정비 영수증">자동차 정비 영수증</Select.Option>
+                        <Select.Option value="병원 진단서">병원 진단서</Select.Option>
+                        <Select.Option value="사고 경위서">사고 경위서</Select.Option>
                     </Select>
                 </Form.Item>
 
-                <Form.Item required={true} label="보험상품 개요"><Input.TextArea name="description" value={state.description} onChange={handleChange}/></Form.Item>
-                <Form.Item required={true} label="기초 보험요율"><InputNumber defaultValue={1.23} min={0} max={100.00} step="0.01" formatter={value => `${value}%`} parser={value => value.replace('%', '')}
-                    // onChange={this.handleChange}
-                /></Form.Item>
+                <Form.Item rules={[{required:true, message: '보험의 기초 보험요율을 입력해주세요'}]} name="basePremiumRate" label="기초 보험요율">
+                    <InputNumber min={0} max={100.00} step="0.01" value={state.basePremiumRate}
+                                 formatter={value => `${value}%`} parser={value => value.replace('%', '')}
+                                 onChange={(val)=>{handleChange({target: {name: 'basePremiumRate', value: val}})}}/>
+                </Form.Item>
 
+                <Form.Item rules={[{required:true, message: '보험의 개괄적인 설명을 입력해주세요'}]} name={"description"} label="보험상품 개요">
+                    <Input.TextArea name="description" value={state.description} onChange={handleChange}/>
+                </Form.Item>
+                <Form.Item required={true} name="createEmployeeId" label="보험을 생성하는 직원 ID" >
+                    <Input readOnly={true} name="createEmployeeId" defaultValue={state.createEmployeeId} value={state.createEmployeeId} disabled={true}/>
+                           {/*onInput={(val)=>{handleChange({target: {name: 'createEmployeeId', value: val}})}}/>*/}
+                </Form.Item>
+
+                <Form.Item rules={[{required: true, message: '담당 직원을 입력해주세요!'}]} name="managementEmployeeId" label="보험 담당책임 직원 ID" >
+                    <Input name="managementEmployeeId" value={state.managementEmployeeId} onChange={handleChange}/>
+                </Form.Item>
                 <Form.Item><Button type="primary" htmlType="submit" value="Submit">Submit</Button></Form.Item>
             </Form>
         </Wrapper>
--- src/pages/Sales/Client.jsx
+++ src/pages/Sales/Client.jsx
@@ -1,10 +1,230 @@
-import React from "react";
+import React, {useState} from "react";
+import {Wrapper} from "../../components/Wrapper";
+import {DataTable2} from "../../components/DataTable2";
+import axios from "axios";
+import useAsync from "../../customHooks/useAsync";
+import "../../css/Detail.css";
+import {Button, Dropdown, DatePicker, Radio, Menu, Space, Tag, Input, Select, Divider} from "antd";
+import {DownOutlined} from "@ant-design/icons";
+import Search from "antd/es/input/Search";
+import styled from "styled-components";
+const { RangePicker } = DatePicker;
+const { Option } = Select;
 
-const Client = () => {
+const StyledSpan = styled.span`
+    font-size: 14px;
+`
+
+async function getClients() {
+    const response = await axios.get(
+        'http://localhost:4000/client'
+    );
+    return response.data;
+}
+
+const Client = ({match, history}) => {
+    const title = "고객관리";
+    const subtitle = "잠재 고객 및 계약 고객을 분석하여 추가 영업기회 확보";
+    const [searchData, setSearchData] = useState([]);
+    const [state, refetch] = useAsync(getClients, setSearchData, [getClients]);
+    const { loading, data, error } = state;
+    const [option, setOption] = useState("고객명");
+
+    const onRow = (record, rowIndex) => {
+        return {
+            onClick: () => {
+                // history.push(`${match.url}/${record.id}`)
+            },
+        };
+    };
+
+    function handleMenuClick(e) {
+        if (e.key === '1')
+        {
+            console.log('click', e.key);
+            setOption("고객명");
+        }
+        else if (e.key === '2')
+        {
+            console.log('click', e.key);
+            setOption("전화번호");
+        }
+    }
+
+    const columns = [
+        {
+            title: 'No',
+            dataIndex: 'id',
+            key: 'id',
+            width: '10%',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: '고객명',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: '전화번호',
+            key: 'phoneNumber',
+            dataIndex: 'phoneNumber',
+            width: '20%',
+        },
+        {
+            title: '가입상태',
+            key: 'id',
+            width: '15%',
+            dataIndex: 'contractList',
+            filters: [
+                {
+                    text: '가입고객',
+                    value: 1
+                },
+                {
+                    text: '미가입고객',
+                    value: 0
+                }],
+            onFilter: (value, record) => {
+                console.log(record.contractList.length);
+                if (value == 0) {
+                    if (record.contractList.length == 0)
+                        return true;
+                    else if (record.contractList.length > 0)
+                        return false;
+                } else if (value == 1) {
+                    if (record.contractList.length == 0)
+                        return false;
+                    else if (record.contractList.length > 0)
+                        return true;
+                }
+            },
+            render: (contractList, data) => {
+                let status;
+                let color;
+                if (contractList.length === 0) {
+                    status = "미가입고객";
+                    color = "volcano";
+                } else if (contractList.length > 0) {
+                    status = "가입고객";
+                    color = "geekblue";
+                }
+                return (
+                    <Tag color={color} key={data.id}>
+                        {status}
+                    </Tag>
+                );
+            }
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            width: '10%',
+            render: (text, record) => (
+                <Space size="middle">
+                    <a style={{color:'purple'}}>Modify</a>
+                </Space>
+            ),
+        },
+    ];
+
+    if (error) {
+        return (
+            <div>
+                에러가 발생하였습니다.
+            </div>
+        );
+    }
+
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item key="1">
+                고객명
+            </Menu.Item>
+            <Menu.Item key="2">
+                전화번호
+            </Menu.Item>
+        </Menu>
+    );
+
+    const onSearch = value => {
+        if (value === "")
+        {
+            setSearchData(data);
+        }
+        else if (option === "전화번호")
+        {
+            console.log("number");
+            console.log(value);
+            let res = [];
+            data.forEach(function (d){
+                if (d.phoneNumber.includes(value))
+                    res.push(d);
+            })
+            setSearchData(res);
+        }
+        else if (option === "고객명")
+        {
+            console.log("name");
+            console.log(value);
+            let res = [];
+            data.forEach(function (d){
+                if (d.name.includes(value))
+                    res.push(d);
+            })
+            setSearchData(res);
+        }
+    };
+
     return (
-        <div>
-            <h1>Client</h1>
-        </div>
+        <Wrapper title={title} underline={true} subtitle={subtitle} >
+            <Space>
+                <Dropdown overlay={menu}>
+                    <Button style={{ width: 95 }}>
+                        {option} <DownOutlined />
+                    </Button>
+                </Dropdown>
+                <Search placeholder="검색할 내용" allowClear onSearch={onSearch} style={{ width: 300 }} />
+                <Divider type="vertical" />
+                <StyledSpan>계약 만기일</StyledSpan><RangePicker />
+                <Divider type="vertical" />
+                <StyledSpan>연령대</StyledSpan>
+                <div className="site-input-group-wrapper">
+                    <Input.Group compact>
+                        <Select defaultValue="1">
+                            <Option value="1">Between</Option>
+                            <Option value="2">Except</Option>
+                        </Select>
+                        <Input style={{ width: 100, textAlign: 'center' }} placeholder="Minimum" />
+                        <Input
+                            className="site-input-split"
+                            style={{
+                                width: 30,
+                                borderLeft: 0,
+                                borderRight: 0,
+                                pointerEvents: 'none',
+                            }}
+                            placeholder="~"
+                            disabled
+                        />
+                        <Input
+                            className="site-input-right"
+                            style={{
+                                width: 100,
+                                textAlign: 'center',
+                            }}
+                            placeholder="Maximum"
+                        />
+                    </Input.Group>
+                </div>
+                <Divider type="vertical" />
+                <Radio.Group value="15px">
+                    <Radio.Button value="남">남</Radio.Button>
+                    <Radio.Button value="여">여</Radio.Button>
+                </Radio.Group>
+            </Space>
+            <DataTable2 dataSource={searchData} columns={columns} loading={loading} onRow={onRow}/>
+        </Wrapper>
     )
 }
 
